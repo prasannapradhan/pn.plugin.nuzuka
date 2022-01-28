ae097c95c90eddac3be891e6cbb37844f614aa38
Pearnode Technology Solutions <11991272+prasannapradhan@users.noreply.github.com>
2022-01-28 20:14:12 +0530

    Update NuzukaLeadGenerationActivator.php

--- includes/NuzukaLeadGenerationActivator.php
+++ includes/NuzukaLeadGenerationActivator.php
@@ -6,19 +6,22 @@
         private static $pc = "__nzk_prof_code__";
         
     	public static function activate($surl) {
-    	    error_log("Inside activate [$surl]");
     	    $rdata = (object) array();
             $rdata->oc = self::$oc;
             $rdata->pc = self::$pc;
             $rdata->surl = $surl;
             
-            error_log("Calling curl url [https://api.pearnode.com/nuzuka/site/plugin/activate.php]");
+            error_log("Request json [".json_encode($rdata)."]");
     	    $ch = curl_init("https://api.pearnode.com/nuzuka/site/plugin/activate.php");
     	    curl_setopt($ch, CURLOPT_POST, 1);
     	    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($rdata));
     	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     	    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    	    curl_setopt($ch, CURLOPT_FAILONERROR, true);
     	    curl_exec($ch);
+    	    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    	    $curl_errno = curl_errno($ch);
+    	    error_log("CURL HTTP status [$http_status] and error number [$curl_errno]");
     	    curl_close($ch);
     	}
     }
